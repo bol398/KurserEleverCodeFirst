924bba2c48a354586e3dc3aa2813cf35fd83699d
Rasmus Lindberg <ragge00@hotmail.se>
2019-09-27 11:19:48 +0200

    1

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using System.Threading;
+
+
+
+namespace SQL
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            using (var schoolContext = new SchoolContext())
+            {
+                var student = new Student();
+                student.Name = "AAA";
+                schoolContext.Students.Add(student);
+                schoolContext.SaveChanges();
+            } 
+        }
+       
+    }
+}
---
+++ Students.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+namespace SQL
+{
+    public class Student
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+
+        public int StudentID { get; set; }
+
+        public string Name { get; set; }
+        
+    }
+
+    public class SchoolContext : DbContext
+    {
+        private const string connectionString = "Server=localhost;Database=EFCore;Trusted_Connection=True";
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+
+        public DbSet<Student> Students { get; set; }
+        
+    }
+}
